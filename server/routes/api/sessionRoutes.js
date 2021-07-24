529863fcaaf70ba63fd07f1bede83561dc32f166
bpeteradams <bpeteradams@gmail.com>
2021-07-24 17:07:58 -0400

    detailed comments in session routes to detail the process of creating a new campaign session

--- server/routes/api/sessionRoutes.js
+++ server/routes/api/sessionRoutes.js
@@ -2,26 +2,33 @@ const router = require('express').Router();
 const { CampaignSession, Campaign } = require('../../models');
 const ExpressError = require('../../utils/expressError');
 
+//route to create a new campaign session in the db
 router.post('/', async (req, res) => {
+  //create a new instance of Campaign Session
   const newCampaignSession = new CampaignSession(req.body)
   // find campaign
   try {
+    //Find Campaign by ID
     const campaign = await Campaign.findById(req.body.campaign);
     if(!campaign){
+      //If no Campaign is found with that id, throw an error
       throw new ExpressError(204, 'No Campaign Found With That ID');
     }
-    console.log(campaign)
+    //set the session number using elements found within campaign
     newCampaignSession.sessionNumber = `${campaign.name}-${campaign.sessionNum}`;
+    //attempt to save the session
     const session = await newCampaignSession.save();
-    console.log('SESSION', session)
+    //increment the campaign's session by 1
     campaign.sessionNum = campaign.sessionNum + 1
-    console.log('CAMPAIGN WITH NEW SESSION NUM')
+    //save the updated campaign to the database
     const savedCampaign = await campaign.save();
+    //send  back both the created session and the updated campaign as json
     res.json({
       sess: session,
       campaign: savedCampaign
     });
   } catch (error) {
+    //if there's an error, send it back as json
     res.json(error)
   }
 })
