01c5a641ec5d4f103a908229c2c6e99ea1916d9e
bpeteradams <bpeteradams@gmail.com>
2021-07-24 16:54:06 -0400

    got sessionNumber set in session model

--- server/routes/api/sessionRoutes.js
+++ server/routes/api/sessionRoutes.js
@@ -4,12 +4,17 @@ const ExpressError = require('../../utils/expressError');
 
 router.post('/', async (req, res) => {
   const newCampaignSession = new CampaignSession(req.body)
-  newCampaignSession.sessionNumber = 1
-  console.log('NEW CAMAPaIGN SESSION', newCampaignSession)
+  // find campaign
   try {
-   const session = await newCampaignSession.save()
-   console.log('SESSION', session)
-    res.json(session)
+    const campaign = await Campaign.findById(req.body.campaign);
+    if(!campaign){
+      throw new ExpressError(204, 'No Campaign Found With That ID');
+    }
+    console.log(campaign)
+    newCampaignSession.sessionNumber = `${campaign.name}-${campaign.sessionNum}`;
+    const session = await newCampaignSession.save();
+    console.log('SESSION', session)
+    res.json(session);
   } catch (error) {
     res.json(error)
   }
