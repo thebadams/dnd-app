52101a9d1ecde78942df32d680f793afa8fdbfda
Brian Adams <bpeteradams@gmail.com>
2021-07-23 22:25:36 -0400

    basic mongo connection set up

---
+++ server/config/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = require('dotenv').config();
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true }, () => {
+  console.log('DATABASE CONNECTED');
+});
+
+module.exports = mongoose;
