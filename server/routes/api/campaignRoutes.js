41c3b1d9a87eedbf48db7657a536c7b9d4c85a68
Brian Adams <bpeteradams@gmail.com>
2021-07-24 00:09:05 -0400

    developed a route to delete a campaign

--- server/routes/api/campaignRoutes.js
+++ server/routes/api/campaignRoutes.js
@@ -5,8 +5,8 @@ const ExpressError = require('../../utils/expressError')
 router.post('/', async (req, res)=> {
   console.log(req)
   try {
-    const newCampaign = await Campaign.create({name: "Galdrheim"});
-    res.json(newCampaign)
+    const newCampaign = await Campaign.create({name: "George"});
+    res.status(201).json(newCampaign)
 
   } catch (error) {
     res.json(error)
@@ -16,14 +16,28 @@ router.post('/', async (req, res)=> {
 
 router.get('/', async (req, res) => {
  try {
-   const campaigns = await Campaign.find()
-   if(!campaigns) {
-     throw new ExpressError(404, 'No Campaigns Found')
+   const campaigns = await Campaign.find().exec()
+   if (!campaigns) {
+     throw new ExpressError(204, 'No Campaigns Found')
    }
    res.json(campaigns)
  } catch (error) {
    res.json(error)
  }
+})
+
+router.delete('/:id', async(req, res) => {
+  console.log(req.params)
+  try {
+    const deletedCampaign = await Campaign.findByIdAndDelete(req.params.id)
+    if (!deletedCampaign) {
+      throw new ExpressError(204, "No Campaigns With That ID Found")
+    }
+    res.status(200).json(deletedCampaign)
+
+  } catch (error) {
+    res.json(error)
+  }
 })
 
 module.exports = router;
