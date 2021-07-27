87724f6981c8c7081488628d209bc386bf56627b
Brian Adams <bpeteradams@gmail.com>
2021-07-27 18:02:06 -0400

    added note model to models/index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,9 @@
 const Campaign = require('./campaign');
 const CampaignSession = require('./campaignSession')
+const Note = require('./note')
 
 module.exports = {
   Campaign,
-  CampaignSession
+  CampaignSession,
+  Note
 }
